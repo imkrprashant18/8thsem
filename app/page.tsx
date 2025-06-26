574d91939ba85219fcbc5592fbfa1ef6f5042de7
imkrprashant18 <prashantmahato77@gmail.com>
2025-06-26 14:55:31 +0545

    clerk

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <>
+      <div className="py-32">
+        <h1>hello doctor app</h1>
+        <Button>Cklicke me</Button>
+      </div>
+    </>
+  );
+}
